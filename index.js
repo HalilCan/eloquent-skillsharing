1504e7a3cc8c249bd4540c125e6d9414c0dd7428
HalilCan <halilcanmemoglu@gmail.com>
2018-09-23 15:27:46 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by hcm on 23.09.2018.
+ */
+/*
+ This project is skillsharing / event planning website. Client-server communication is done through http requests.
+ However, I will use long polling for instantaneous updates.
+ */
+
