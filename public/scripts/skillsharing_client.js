d18ef0431c76dd667b25d3c371af22241506b19d
HalilCan <halilcanmemoglu@gmail.com>
2018-11-06 23:45:04 -0500

    Forum 0.2.0
    Done:
    Warning fixes
    
    WIP:
    
    Planned

--- public/scripts/skillsharing_client.js
+++ public/scripts/skillsharing_client.js
@@ -169,6 +169,7 @@ function elt(type, props, ...children) {
     return dom;
 }
 
+function renderTalk(talk, dispatch, typedComment) {
     return elt(
         "section", {className: "talk"},
         elt("section", {className: "talkHeader"}, elt("section", {className: "talkTitle"},
